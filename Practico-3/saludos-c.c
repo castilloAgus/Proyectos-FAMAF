999d1107b170c3c912e3e91d7428bf86a1ddb678
castilloAgus <155190404+castilloAgus@users.noreply.github.com>
2024-03-16 14:56:36 -0300

    Create saludos-c.c

---
+++ Practico-3/saludos-c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void imprimir_hola(){
+
+    printf("hola\n");
+}
+
+void imprimir_chau(){
+
+    printf("chua\n");
+}
+
+int main(void){
+
+    imprimir_hola();
+    imprimir_hola();
+
+    imprimir_chau();
+    imprimir_chau();
+
+    return 0;
+}
