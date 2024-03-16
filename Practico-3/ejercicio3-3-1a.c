54d3cfb6b38a35751369b213dfcad51de1e37608
castilloAgus <155190404+castilloAgus@users.noreply.github.com>
2024-03-16 14:43:40 -0300

    Create ejercicio3-3-1a.c

---
+++ Practico-3/ejercicio3-3-1a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    printf("ingresa un valor para x: \n");
+     scanf("%d", &x);
+     x=5;
+    printf("ahora el valor lamda: %d\n", x);
+     
+    return 0;
+}
+/* ingresa un valor para x:
+2
+ahora el valor lamda: 5
+
+ingresa un valor para x:
+4
+ahora el valor lamda: 5
+
+ingresa un valor para x:
+7
+ahora el valor lamda: 5
+
+ */
