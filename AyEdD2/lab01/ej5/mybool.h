834118bfa386a845e357dd376fca3491ef1449c9
castilloAgus <155190404+castilloAgus@users.noreply.github.com>
2024-03-28 20:36:32 -0300

    Add files via upload

---
+++ AyEdD2/lab01/ej5/mybool.h
@@ -0,0 +1,18 @@
+#ifndef __cplusplus
+
+#ifndef bool
+#define bool char
+#endif 
+
+#ifndef true
+#define true 1
+#endif 
+
+#ifndef false
+#define false 0
+#endif
+
+#endif
+
+typedef int mybool;
+
