b051103a7219dd4a14d1c68763ede2753111de3b
castilloAgus <155190404+castilloAgus@users.noreply.github.com>
2024-03-28 20:32:41 -0300

    Add files via upload

---
+++ AyEdD2/lab01/ej3/array_helpers.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "array_helpers.h"
+
+void array_dump(int a[], unsigned int length) {
+		printf("[");
+    for (unsigned int i=0; i<length; i++) {
+        printf("%d",a[i]);
+		if(i<length-1){
+			printf(",");
+		}
+    }
+    printf("]\n");
+}
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath) {
+	   FILE*archivo;
+	   archivo = fopen(filepath, "r");
+	   unsigned int arreglo; 
+	   fscanf(archivo,"%u", &arreglo);
+	   if (max_size <= arreglo) {
+	   	return 0;
+	   }
+	   for (unsigned int i=0; i<arreglo; i++) {
+	   	fscanf(archivo,"%d", &array[i]);
+	   }
+	   fclose (archivo);
+	   return arreglo;
+	
+}
