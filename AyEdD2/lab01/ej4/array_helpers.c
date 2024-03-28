86fe328af4328f6dadc2e24db1382f36da9d9413
castilloAgus <155190404+castilloAgus@users.noreply.github.com>
2024-03-28 20:35:07 -0300

    Add files via upload

---
+++ AyEdD2/lab01/ej4/array_helpers.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "array_helpers.h"
+
+void array_dump(int a[], unsigned int length) {
+		printf("[");
+    for (unsigned int i=0; i<length; i++) {
+        printf("%d",a[i]);
+		if(i<length-1){
+			printf(",");
+		}
+    }
+    printf("]\n");
+}
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath) {
+	   FILE*archivo;
+	   archivo = fopen(filepath, "r");
+	   unsigned int arreglo; 
+	   fscanf(archivo,"%u", &arreglo);
+	   if (max_size <= arreglo) {
+	   	return 0;
+	   }
+	   for (unsigned int i=0; i<arreglo; i++) {
+	   	fscanf(archivo,"%d", &array[i]);
+	   }
+	   fclose (archivo);
+	   return arreglo;
+	
+}
+
+bool array_is_sorted(int a[], unsigned int length){
+	for(unsigned int i=0; i<length-1; i++){
+		if(a[i]>a[i+1]){
+			return false;
+		}
+	}
+	return true;
+}
